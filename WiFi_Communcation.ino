7398dc798a3e7bb665a2d4041d5b3569e946d157
lucashkaplan <55100296+lucashkaplan@users.noreply.github.com>
2019-11-06 14:27:34 -0500

    Add files via upload

---
+++ WiFi_Communcation.ino
@@ -0,0 +1,38 @@
+#include "Enes100.h"
+#include "Tank.h"
+
+int markerid = 5;
+int wifitx = 2, wifirx = 3; 
+
+void setup() {
+  //Team Name, Mission Type, Marker ID, TX Pin, RX Pin
+  Enes100.begin("Water Those", WATER, markerid, wifitx, wifirx);
+   
+	Enes100.print("Destination is at (");
+  Enes100.print(Enes100.destination.x);
+  Enes100.print(", ");
+  Enes100.print(Enes100.destination.y);
+  Enes100.println(")");
+
+	
+}
+
+void loop() {
+	 if (Enes100.updateLocation()) {
+        Enes100.print("OSV Location: (");
+        Enes100.print(Enes100.location.x);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.y);
+        Enes100.print(", ");
+        Enes100.print(Enes100.location.theta);
+        Enes100.println(")");
+        delay(500);
+    } 
+    else {
+        // OSV's location was not found
+        Enes100.println("Error 404! OSV Not Found!");
+    }
+	
+	
+	
+}
